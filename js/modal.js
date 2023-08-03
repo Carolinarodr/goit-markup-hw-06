68b23cfa7dfe53d3b6a6bc775297c4722deb2912
Carolinarodr <dianita2398rodri@gmail.com>
2023-08-02 22:00:17 -0500

    correciones

--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,13 @@
   const refs = {
     openModalBtn: document.querySelector('.button-primary'),
     closeModalBtn: document.querySelector('.container-svg'),
-    modal: document.querySelector('.modal-backdrop'),
+    modal: document.querySelector('.is-hidden'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("modal-backdrop");
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
